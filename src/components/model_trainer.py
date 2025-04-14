dcbc54945192d2bccee997b0f1c4736e47416b4e
Mouadkeddis <mouadkeddis430@gmail.com>
2025-04-14 12:43:52 +0200

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dataclasses import dataclass
 
+
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -17,7 +18,7 @@ from xgboost import XGBRegressor
 from src.logger import logging
 from src.exception import CustomException
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 @dataclass
 class ModeltrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
@@ -26,7 +27,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModeltrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array , preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("split  training and test input data")
             X_train, y_train,X_test, y_test =(
@@ -47,5 +48,32 @@ class ModelTrainer:
                 "Catboosting classifier" : CatBoostRegressor(verbose=False),
 
             }
-        except:
-            pass
+            model_report:dict=evaluate_models(X_train, y_train, X_test, y_test, models=models)
+            #TO get the best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+            
+            #TO get the best model name from dict
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+                ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found is {best_model_name} with score {best_model_score}")
+
+            # save the best model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    
